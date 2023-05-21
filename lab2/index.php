ef686de51f2108c4430a5c92ba955a93ddacf6cc
Claire Wanjia Tang <113147894+tangwanjia@users.noreply.github.com>
2023-05-21 13:29:40 -0400

    Add files via upload

---
+++ lab2/index.php
@@ -0,0 +1,26 @@
+
+        <?php  
+            include "template.php";
+
+            function $indexNum 
+           
+
+            
+            //foreach($invoices as $invoice ){
+              //  echo "<tr>";
+              //  echo "<td>" . $invoice['number'] ."</td>";
+              //  echo "<td>" . $invoice['amount'] . "</td>";
+              //  echo "<td>" . $invoice['status'] . "</td>";
+              //  echo "<td>" . $invoice['client'] . "</td>";
+              //  echo "<td>" . $invoice['email'] . "</td>";
+                echo "</tr>";
+            //}
+        ?>
+
+    </table>
+        <br>
+
+        
+        
+    </body>
+</html>
